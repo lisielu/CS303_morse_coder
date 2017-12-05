d66c0d5980cbc71896338aa1577a2daa09e0941f
lisielu <elise.fairfax@gmail.com>
2017-12-05 16:49:46 -0600

    Update: BTNode / Debug

--- morse_coder.h
+++ morse_coder.h
@@ -15,35 +15,38 @@
 
 
 //"MBTNode" stands for Morse Bin Tree
-template <typename T>
-class MBTNode{
-public:
-	
+template <typename Item_Type>
+struct BTNode{
   //data fields
-  MBTNode* left;
-  MBTNode* right;
-  T data;
-  //constructors
-  MBTNode(){
-	  left = NULL;
-	  right = NULL;
-  };
-  MBTNode(MBTNode* leftChild,MBTNode* rightChild,T datum){
-    left = leftChild;
-    right = rightChild;
-    data = datum;
+	Item_Type data;
+  	BTNode<Item_Type>* left;
+  	BTNode<Item_Type>* right;
+  //constructor
+  BTNode(const Item_Type& the_data, BTNode<Item_Type>* left_val = NULL, 
+  	BTNode<Item_Type>* right_val = NULL) : data(the_data), left(left_val), right(right_val) {}
+  //destructor
+  virtual ~BTNode() {}
+  //to_string
+  virtual std::string to_string() const {
+  	std::ostringstream os;
+	os << data;
+	return os.str();
   }
 };
-
+//Overload ostream insertion operator
+template<typename Item_Type>
+std::ostream& operator<<(std::ostream& out, const BTNode<Item_Type>& node){
+	return out << node.to_string();
+}
 
 
 class morse_coder{
   //private data fields
+ private:
   std::map<char,std::string> encoder_dict;
-  std::map<std::string,char> decoder_dict;
-  //encoded or decoded result
+  //encoded or decode result
   std::string result;
-  MBTNode<char>* Root;
+  BTNode<char>* Root;
 public:
   //constructor
   morse_coder();
@@ -51,16 +54,14 @@ public:
   //converts english to morse code
   std::string decode(const std::string& input);
     //decode helper function
-    void find_in_tree(char& result,std::string& input,MBTNode<char>* local_root);
-    
-    std::string translate(const std::string input);
+    void find_in_tree(char& result, std::string& input, BTNode<char>* local_root);
 
   //converts morse code to english variant
   std::string encode(const std::string& input);
     
   //returns a pointer to the root
-    void build_Mtree(MBTNode<char>* local_root,std::ifstream& fin);
-    void place_Node(const char value,MBTNode<char>* local_root,std::string& input);
+    void build_Mtree(BTNode<char>* local_root,std::ifstream& fin);
+    void place_Node(const char value, BTNode<char>* local_root, std::string& input);
 
-
+    std::string translate(const std::string input);
 };
